fb60851975f1cb55070fa38d1492824a7b7fb87d
USER <USER@LAPTOP-JP1P2SCH>
2024-07-09 15:55:31 +0900

    test

--- codingTest/src/edu/codingTest.java
+++ codingTest/src/edu/codingTest.java
@@ -11,10 +11,10 @@ import java.util.Map;
 public class codingTest {
 
 	/**
-	 * 
+	 *
 	문제 : 최빈값 : 주어진 값 중 가장 자주 나오는 값
 	정수 배열 array가 매개변수로 주어질 때,
-	최빈값을 return 하도록 solution 함수를 완성해보세요. 
+	최빈값을 return 하도록 solution 함수를 완성해보세요.
 	최빈값이 여러 개면 -1을 return 합니다.
 	[제한사항]
 	1. 0 < array의 길이 < 100
@@ -50,13 +50,12 @@ public class codingTest {
 		} else {
 			answer = 0;
 		}
-
 		return answer;
 	}
-	
+
 	/**
 	 *
-	힘이나는 커피는 아이스아메리카노를 시켜먹으면 한 잔당 쿠폰을 한 장 발급합니다. 
+	힘이나는 커피는 아이스아메리카노를 시켜먹으면 한 잔당 쿠폰을 한 장 발급합니다.
 	쿠폰을 열 장 모으면 아이스아메리카노를 1잔 서비스로 받을 수 있고, 서비스 에도 쿠폰이 발급됩니다.
 	시켜먹은 아아의 수 coffee가 매개변수로 주어질 때 받을 수 있는 최대 서비스 아아의 수를 return하도록 solution 함수를 완성해주세요.
 	[제한사항]
@@ -74,18 +73,16 @@ public class codingTest {
 
 		return answer;
 	}
-	
+
 	public String solution3(int[][] score) {
 		int[] answer = {};
 		List<Integer> scoreList = new ArrayList<>();
 		boolean flag = true;
 		for (int[] t : score) {
 			if (t[0] >= 0 && t[0] <= 100 && t[1] >= 0 && t[1] <= 100) {
-
 				scoreList.add(t[0] + t[1]);
 			} else {
 				flag = false;
-
 			}
 		}
 
@@ -102,7 +99,7 @@ public class codingTest {
 		}
 		return Arrays.toString(answer);
 	}
-	
+
 	public long solution4(String numbers) {
 		long answer = 0;
 		String[] numbers_arr = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
@@ -113,10 +110,10 @@ public class codingTest {
 		answer = Long.parseLong(numbers);
 		return answer;
 	}
-	
+
 	public int solution5(int[] arr1, int[] arr2) {
 		int answer = 0;
-		
+
 		if(arr1.length != arr2.length) {
 			answer = arr1.length > arr2.length ? 1 : -1;
 		} else {
@@ -128,7 +125,7 @@ public class codingTest {
 			for (int b : arr2) {
 				arr2Sum += b;
 			}
-			
+
 			if(arr1Sum != arr2Sum) {
 				answer = arr1Sum > arr2Sum ? 1 : -1;
 			}else {
@@ -137,7 +134,7 @@ public class codingTest {
 		}
 		return answer;
 	}
-	
+
 	public int[] solution7(int[] arr) {
 
 		// 배열 길이가 1인 경우 -1 반환
@@ -149,7 +146,7 @@ public class codingTest {
 		int[] answer = new int[arr.length - 1];
 
 		int min = arr[0];
-		
+
 		// 가장 작은 수 구하기
 		for (int i = 0; i < arr.length; i++) {
 			min = Math.min(min, arr[i]);
@@ -159,14 +156,14 @@ public class codingTest {
 
 		for (int i = 0; i < arr.length; i++) {
 			if (arr[i] == min) {
-				continue;
 				//continue for문으로 올라감
+				continue;
 			}
 			answer[index++] = arr[i];
 		}
 		return answer;
 	}
-	
+
 	public String solution8(String new_id) {
 		String answer = "";
 		String temp = new_id.toLowerCase();
@@ -186,13 +183,12 @@ public class codingTest {
 			while (temp.length() < 3) {
 				temp += temp.charAt(temp.length() - 1);
 			}
-
 		}
 		answer = temp;
 
 		return answer;
 	}
-	
+
 	public String solution9(String s) {
 		String answer = "";
 		String[] str = s.split("");
@@ -204,7 +200,7 @@ public class codingTest {
 		}
 		return answer;
 	}
-	
+
 	public String solution10(String s) {
 		String answer = "";
 		String[] numbers = s.split(" ");
@@ -223,39 +219,39 @@ public class codingTest {
 		answer = min + " " + max;
 		return answer;
 	}
-	
+
 	public static void main(String[] args) {
 
 		codingTest main = new codingTest();
-		
+
 		int[] test1 = {1, 4, 3, 4, 8};
 //		System.out.println(main.solution1(test1));	//최빈값 구하기
-		
+
 		int test2 = 89;
 //		System.out.println(main.solution2(test2));	//증감, 몫
-		
+
 		int[][] test3 = { { 100, 70 }, { 100, 100 }, { 100, 100 } };
 //		System.out.println(main.solution3(test3));	//2차원 배열
-		
+
 		String test4 = "zerothree";
 //		System.out.println(main.solution4(test4));
-		
+
 		int[] test5 = {100,17,84,1};
 		int[] test6 = {55,12,65,36};
 //		System.out.println(main.solution5(test5, test6));
-		
+
 		int[] test7 = { 10, 2 , 3, 2, 55};
 //		System.out.println(Arrays.toString((main.solution7(test7))));
-		
+
 		String test8 = "bassSY";
 //		System.out.println(main.solution8(test8));
-		
+
 		String test9 = "Zakeidk";
 //		System.out.println(main.solution9(test9));
-		
+
 		String test10 = "-1 -2 3 4";
 //		System.out.println(main.solution10(test10));
-		
+
  	}
 
 }
